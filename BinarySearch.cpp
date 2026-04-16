693b823b29e0a365bb93281a85b2f098dc235ca9
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-16 17:21:18 +0700

    Menambahkan input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int element [10];
-int npanjang;
+int elemen[10];
+int nPanjang;
 int x;
 
+void input(){
+    while(true){
+        cout << "Masukan banyak elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10){
+            break;
+        }
+        else {
+            cout << "\n[!] Jumalah elemen tidak boleh lebih dari 10, silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n=============================\n";
+    cout << "\n    Masukan Elemen Array    \n";
+    cout << "\n=============================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Elemen ke-" << i+1 << ": ";

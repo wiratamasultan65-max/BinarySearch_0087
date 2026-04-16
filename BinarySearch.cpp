03cde536b1c2a50b80f4e7e16b8b603606b95ba2
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-16 18:06:44 +0700

    Menambahkan Fungsi pencarian binary

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -50,3 +50,74 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1);
 }
 
+void display()
+{
+  cout << "\n=======================================\n";
+  cout << "   elemen array setelah diurutkan          \n";
+  cout << "\n=======================================\n";
+
+  for (int j = 0; j < nPanjang; j++)
+  {
+    cout << element[j];
+    if (j < nPanjang - 1)
+    {
+      cout << "->";
+    }
+  }
+  cout << endl;
+}
+void binarysearch()
+{
+    char ulang;
+    do
+    {
+        cout << " \n=======================================\n";
+        cout << "         Pencarian Binary  Search        \n";
+        cout << "\n=======================================\n";
+        cout << "masukkan elemen yang ingin di cari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (x == element[mid])
+            {
+                cout << "elemen " << x << " ditemukan pada indeks ke-" << mid << endl;
+                break;
+            }
+            else if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+}
+
+int main()
+{
+  input();
+  bubbleSortArray();
+  display();
+  binarysearch();
+
+  return 0;
+}
+
